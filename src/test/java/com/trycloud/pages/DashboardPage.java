bc806cc06c44deb0c6cada2d15b72a29bd51012e
Kristina Luna <93393618+KristinaLuna@users.noreply.github.com>
2022-03-27 19:54:32 -0400

    selectModule method in DashboardPage.java

--- src/test/java/com/trycloud/pages/DashboardPage.java
+++ src/test/java/com/trycloud/pages/DashboardPage.java
@@ -1,4 +1,20 @@
 package com.trycloud.pages;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 public class DashboardPage {
+
+    //this method loops through all modules on the homepage and selects the one you pass in String attributeValue
+    public static void selectModule(List<WebElement> appMenuModules, String attributeValue){
+
+        for (WebElement each : appMenuModules) {
+
+            if (each.getAttribute("aria-label").equalsIgnoreCase(attributeValue)){
+                each.click();
+            }
+        }
+
+    }
 }
