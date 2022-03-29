32834f6d292ce6f04fbdced469c05c953bdd04db
humas <humashinwari@gmail.com>
2022-03-28 23:30:35 -0400

    US7 started

--- src/test/java/com/trycloud/step_definitions/ManageFolders_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/ManageFolders_StepDefinitions.java
@@ -33,15 +33,15 @@ public class ManageFolders_StepDefinitions {
     }
     @When("user click \"new folder”")
     public void user_click_new_folder() {
-
+        files.addNewFolder.click();
     }
     @When("user write a folder name")
     public void user_write_a_folder_name() {
-
+        files.inputFolderName.sendKeys("Testing");
     }
     @When("the user click submit icon")
     public void the_user_click_submit_icon() {
-
+        files.submit.click();
     }
     @Then("Verify the folder is displayed on the page")
     public void verify_the_folder_is_displayed_on_the_page() {
