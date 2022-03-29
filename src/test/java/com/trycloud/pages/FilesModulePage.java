1ad0f4b209f0aaa16aa30fb9ffb6370ac3c18f31
irena stanojlovic <stanojlovicirena@gmail.com>
2022-03-28 23:51:15 -0400

    Irena initial commit

--- src/test/java/com/trycloud/pages/FilesModulePage.java
+++ src/test/java/com/trycloud/pages/FilesModulePage.java
@@ -13,4 +13,19 @@ public class FilesModulePage extends BasePage {
     @FindBy(xpath="//div[@id='app-settings-content']//label")
     public List<WebElement> checkBoxes;
 
+    @FindBy(xpath = "//a[@class='action action-menu permanent']")
+    public List<WebElement> actionIcon;
+
+    @FindBy(xpath = "//span[.='Remove from favorites']")
+    public WebElement removeFromFavorites;
+
+    @FindBy(xpath = "//a[.='Favorites']")
+    public WebElement favoritesSubmodule;
+
+    @FindBy(xpath = "//a[@class='button new']")
+    public WebElement addFileButton;
+
+    @FindBy(xpath = "//span[.='Upload file']")
+    public WebElement uploadFileButton;
+
 }
