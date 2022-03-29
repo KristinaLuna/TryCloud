8c2c9ac79f9433241599ce39b6faed848db11fd7
YanchunZhou <tifa1981@gmail.com>
2022-03-28 23:49:54 -0400

    new locators added for files page.

--- src/test/java/com/trycloud/pages/FilesModulePage.java
+++ src/test/java/com/trycloud/pages/FilesModulePage.java
@@ -10,7 +10,20 @@ public class FilesModulePage extends BasePage {
     @FindBy(css = "button[class='settings-button']")
     public WebElement settingsBtn;
 
-    @FindBy(xpath="//div[@id='app-settings-content']//label")
+    @FindBy(xpath = "//div[@id='app-settings-content']//label")
     public List<WebElement> checkBoxes;
+
+    @FindBy(xpath = "(//p[text()])[1]")
+    public WebElement numberOfUsage;
+
+    @FindBy(css = "span[class='icon icon-add']")
+    public WebElement icon_add;
+
+    @FindBy(xpath = "(//span[@class='displayname'])[1]")
+    public WebElement uploadFileBtn2;
+
+    @FindBy(xpath = "//span[@class='svg icon icon-upload']")
+    public WebElement uploadFileBtn;
+
 
 }
