b87f15c20e318e3b789c4f9fc97c81996dd05e5b
Giuliana Agreda <giuliana_agreda@hotmail.com>
2022-03-29 21:49:41 -0400

    US1_created and new StepDefinition_added

--- src/test/java/com/trycloud/step_definitions/US1_US2_Login_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/US1_US2_Login_StepDefinitions.java
@@ -12,6 +12,16 @@ public class US1_US2_Login_StepDefinitions {
 
     LoginPage login = new LoginPage();
 
+    @When("user use username {string} and passcode {string}")
+    public void user_use_username_and_passcode(String string, String string2) {
+
+    }
+    @Then("verify the user should be at the dashboard page")
+    public void verify_the_user_should_be_at_the_dashboard_page() {
+
+    }
+
+
     @Given("user on the login page")
     public void user_on_the_login_page() {
         String url = ConfigurationReader.getProperty("trycloud.net.url");
