eb1980ec881afc7d54e26fc4b25f6759e07e86cb
Kristina Luna <93393618+KristinaLuna@users.noreply.github.com>
2022-04-03 11:41:01 -0400

    US12 work in progress

--- src/test/java/com/trycloud/step_definitions/US12_VerifyPageTitle.java
+++ src/test/java/com/trycloud/step_definitions/US12_VerifyPageTitle.java
@@ -1,26 +1,19 @@
 package com.trycloud.step_definitions;
 
-import com.trycloud.pages.DashboardPage;
-import com.trycloud.utilities.Driver;
+
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import java.util.List;
+
 
 public class US12_VerifyPageTitle {
 
 
-    @When("the user clicks the {string} module")
-    public void the_user_clicks_the_module(String string) {
+    @When("the user clicks the {string} module US12")
+    public void the_user_clicks_the_module_us12(String string) {
 
-
-        List<WebElement> allModules = Driver.getDriver().findElements(By.xpath("//*[@id=\"appmenu\"]/li"));
-        DashboardPage.selectModule(allModules, "Contacts");
     }
-    @Then("verify the page title is {string}")
-    public void verify_the_page_title_is(String string) {
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Contacts - Trycloud QA");
+    @Then("verify the page title is {string} US12")
+    public void verify_the_page_title_is_us12(String string) {
+
     }
 }
