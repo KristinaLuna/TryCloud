77f70637d2f05b3a1a213a774a09ab51befbca0b
Giuliana Agreda <giuliana_agreda@hotmail.com>
2022-04-05 11:18:25 -0400

    us1_added US1 to steps and feature. Please review.

--- src/test/java/com/trycloud/step_definitions/US1_Login_StepDefinitions.java
+++ src/test/java/com/trycloud/step_definitions/US1_Login_StepDefinitions.java
@@ -12,27 +12,27 @@ public class US1_Login_StepDefinitions {
 
     LoginPage login = new LoginPage();
 
-    //@Given("user on the login page")
-    //public void user_on_the_login_page() {
+    @Given("user on the login page US1")
+    public void user_on_the_login_page() {
       // String url = ConfigurationReader.getProperty("trycloud.net.url");
        //Driver.getDriver().get(url);
-        //Driver.getDriver().get(ConfigurationReader.getProperty("trycloud.net.url"));
-    //}
+        Driver.getDriver().get(ConfigurationReader.getProperty("trycloud.net.url"));
+    }
 
 
-    @When("user use username {string} and passcode {string}")
+    @When("user use username {string} and passcode {string} US1")
     public void user_use_username_and_passcode(String username, String password) {
         login.inputUsername.sendKeys(username);
         login.inputPassword.sendKeys(password);
 
     }
 
-   // @When("user click the login button")
-    //public void user_click_the_login_button() {
-       // login.submitButton.click();
-    //}
+    @When("user click the login button US1")
+    public void user_click_the_login_button() {
+       login.submitButton.click();
+    }
 
-    @Then("verify the user should be at the dashboard page")
+    @Then("verify the user should be at the dashboard page US1")
     public void verify_the_user_should_be_at_the_dashboard_page() {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("dashboard"));
 
--- src/test/resources/features/US1_login.feature
+++ src/test/resources/features/US1_login.feature
@@ -1,11 +1,11 @@
 Feature: As a user, I should be able to log in.
 
-
+@wip
   Scenario Outline: Verify login with valid credentials
-    Given user on the login page
-    When user use username "<username>" and passcode "<password>"
-    And user click the login button
-    Then verify the user should be at the dashboard page
+    Given user on the login page US1
+    When user use username "<username>" and passcode "<password>" US1
+    And user click the login button US1
+    Then verify the user should be at the dashboard page US1
     Examples:
       | username | password |
       |user7     |Userpass123|
