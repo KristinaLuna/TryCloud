a76bf3534e79afe1fecccb7a960d113e89ebd879
enes3553 <93152484+enes3553@users.noreply.github.com>
2022-04-05 11:55:48 -0400

    US-3

--- src/test/java/com/trycloud/step_definitions/US3_TestHomePageModules_StepDef.java
+++ src/test/java/com/trycloud/step_definitions/US3_TestHomePageModules_StepDef.java
@@ -1,6 +1,7 @@
 package com.trycloud.step_definitions;
 
 
+import com.trycloud.pages.DashboardPage;
 import com.trycloud.pages.FilesModulePage;
 import com.trycloud.pages.LoginPage;
 import io.cucumber.java.en.Then;
@@ -17,7 +18,7 @@ public class US3_TestHomePageModules_StepDef {
 
     LoginPage loginPage = new LoginPage();
     FilesModulePage filesModulePage = new FilesModulePage();
-
+    DashboardPage dashboardPage = new DashboardPage();
 
     @When("the user log in with valid {string} and {string}")
     public void theUserLogInWithValidAnd(String username, String password) {
@@ -27,7 +28,7 @@ public class US3_TestHomePageModules_StepDef {
     @Then("verify the user see the following modules:")
     public void verifyTheUserSeeTheFollowingModules(List<String> expectedModuleNames) {
         List<String> actualModuleNames = new ArrayList<>();
-        for (WebElement eachModule : filesModulePage.modules) {
+        for (WebElement eachModule : dashboardPage.modules) {
             actualModuleNames.add(eachModule.getAttribute("aria-label"));
         }
         assertEquals(expectedModuleNames, actualModuleNames);
